bb6cd602c2a4a271869928766169c0fa74ac2222
Alex Spencer <aspencer@byu.edu>
2013-03-18 16:01:57 -0600

    why did it make me add

--- alejandro2.php
+++ alejandro2.php
@@ -1,1 +1,12 @@
-// pull before push erases this?
+//TEACHING SAMMY T (PHLY'NN'E)
+/*
+
+  562  git status
+  569  vim alejandro2.php
+  570  git add alejandro2.php 
+  571  git pull
+  576  git commit -m 'alejandro2'
+  577  git push
+
+
+*/
