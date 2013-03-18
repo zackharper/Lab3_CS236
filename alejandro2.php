9fb525792ea5029fcbba88bf6e061995c5182dcc
Alex Spencer <aspencer@byu.edu>
2013-03-18 16:34:35 -0600

    tutorial stuff

--- alejandro2.php
+++ alejandro2.php
@@ -10,4 +10,26 @@
 git check­out -- octoc­at.txt means checkout last version of octocat file
 testblah`:wq
 
+
+
+
+
+
+
+//FROM TUTORIAL:
+
+$ git branc­h clean­_up
+Success!
+$ git check­out clean­_up
+Success!
+$ git rm '*.tx­t'
+Success!
+$ git commi­t -m "Remo­ve all the cats"­
+Success!
+$ git check­out maste­r
+Success!
+$ git merge­ clean­_up
+Success!
+$ git branc­h -d clean­_up
+$  
 */
