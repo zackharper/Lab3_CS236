fbcb880e0e99a58c05f1cae3266ae3ac51e3500e
Sam Turner <Sammyt1031@gmail.com>
2013-03-19 13:04:42 -0600

    changed relation

--- Lab2New/Relation.cpp
+++ Lab2New/Relation.cpp
@@ -3,7 +3,10 @@
 #include "Relation.h"
 
 Relation::Relation(Scheme * data){
-    vector<Token*> header_tokens = data->getTokens();//wait...do i need to copy each token? or is this done automatically?
+    vector<Token*> header_tokens(data->getTokens().begin(),data->getTokens().end()); //= data->getTokens();//wait...do i need to copy each token? or is this done automatically?
+    /*for (int i = 0; i < data->getTokens.size(); i++){
+        header_tokens.push_back(data->getTokens()[i]);
+     }*/
     name = header_tokens[0];//first token will be the id that is the name of the relation. should we check for invalid first tokens? probably not, because it will have been checked for when parsing
     columns = new Schema(header_tokens);
 }
